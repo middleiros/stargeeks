bfe209af36071da3aa1620412ced8f24c6c96ab8
Medeiros <gustavo.silva125@portalsesisp.org.br>
2023-05-16 14:51:24 -0300

    Logica 16052023

--- login.js
+++ login.js
@@ -4,5 +4,17 @@ const senha = document.getElementById("senha");
 const formulario = document.getElementById("formulario");
 
 formulario.onsubmit = (evento) => {
-    let dados = JSON.parse(localStorage.getItem)
-}
+    let dados = JSON.parse(localStorage.getItem("base"));
+
+    dados.array.ForEach((elemento) => {
+        if (elemento.email == email.value && elemento.senha == senha.value) {
+            evento.preventDefault();
+            return true;
+        } else {
+            mensagem.innerHTML = "Senha ou Email incorreto."
+         }
+    }
+  
+       
+ }
+};
