8a9ac31dbbe40413a7a8c41e25c9abd0bb6f61b0
Medeiros <gustavo.silva125@portalsesisp.org.br>
2023-05-16 07:50:19 -0300

    Logica Inicial

---
+++ login.js
@@ -0,0 +1,8 @@
+const mensagem = document.querySelector("mensagem");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const formulario = document.getElementById("formulario");
+
+formulario.onsubmit = (evento) => {
+    let dados = JSON.parse(localStorage.getItem)
+}
